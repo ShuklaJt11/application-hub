52e6a75e0c14ee0bc725ac5f1038dd5030784cb3
Jatin Shukla <jatinshukla.lek@gmail.com>
2026-02-14 14:34:50 +0530

    #1: Fixed linting issues

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,28 +1,31 @@
+import asyncio
 from logging.config import fileConfig
-from sqlalchemy import pool
-from sqlalchemy.ext.asyncio import AsyncEngine
-from alembic import context
-from app.db.base import Base
+
+from alembic import context  # type: ignore[attr-defined]
 from app.core.config import settings
+from app.db.base import Base
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import create_async_engine
 
 config = context.config
 fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
 
-def run_migrations_online():
-    connectable = AsyncEngine(
-        create_async_engine(settings.DATABASE_URL, poolclass=pool.NullPool)
-    )
+
+async def run_migrations_online():
+    connectable = create_async_engine(settings.DATABASE_URL, poolclass=pool.NullPool)
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
 
     await connectable.dispose()
 
-async def do_run_migrations(connection):
+
+def do_run_migrations(connection):
     context.configure(connection=connection, target_metadata=target_metadata)
     with context.begin_transaction():
         context.run_migrations()
 
-run_migrations_online()
+
+asyncio.run(run_migrations_online())
